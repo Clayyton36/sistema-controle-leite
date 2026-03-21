2a6647bab9b164774abaf5aa7839a68d7108498d
Clayton <seuemail@exemplo.com>
2026-03-20 21:46:59 -0300

    Melhoria visual com Bootstrap e padronização das telas

--- entregas/forms.py
+++ entregas/forms.py
@@ -6,3 +6,9 @@ class EntregaForm(forms.ModelForm):
     class Meta:
         model = Entrega
         fields = ['morador', 'data_entrega', 'quantidade']
+
+        widgets = {
+            'morador': forms.Select(attrs={'class': 'form-control'}),
+            'data_entrega': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'quantidade': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
--- moradores/forms.py
+++ moradores/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Morador
 
-
 class MoradorForm(forms.ModelForm):
     class Meta:
         model = Morador
         fields = ['nome', 'cpf', 'telefone', 'endereco', 'ativo']
+
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+            'endereco': forms.TextInput(attrs={'class': 'form-control'}),
+            'ativo': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
